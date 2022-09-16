79a783040f23ca702cb10a0dfc30d614a8cd94bf
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-09-16 21:48:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+
+// menggunakan configurasi enkripsi dotenv
+dotenv.config();
+
+// melakukan koneksi ke mongoDB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Berhasil melakukan koneksi ke MongoDB"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// API end point
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+// PORT Backend
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server Backend Berjalan!!");
+});
