e3e6ad6824a611d3a3d9bcdcbe64224537692f78
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-11-08 11:38:12 +0700

    update firebase config

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,16 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCB-h_T2ZL-a7xt5zMvZmSh8GBXtawzL0c",
-  authDomain: "shop-889c6.firebaseapp.com",
-  projectId: "shop-889c6",
-  storageBucket: "shop-889c6.appspot.com",
-  messagingSenderId: "105110075028",
-  appId: "1:105110075028:web:7cee5adf1ed39f597e6681",
+  apiKey: "AIzaSyB5gLVCUZ7C_NX-SR8OUk1Y8iWJofQdLx4",
+  authDomain: "padjadjaranmart.firebaseapp.com",
+  projectId: "padjadjaranmart",
+  storageBucket: "padjadjaranmart.appspot.com",
+  messagingSenderId: "873802740060",
+  appId: "1:873802740060:web:344f0b3415cff6e8da1199",
+  measurementId: "G-5DMKEHHM3W",
 };
 
 // Initialize Firebase
